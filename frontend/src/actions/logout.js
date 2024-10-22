a9e0ee8e2ffde09ad9b928c9d3844912bac1fa13
ThagIspace <97510320008@ispace.edu.vn>
2024-10-22 22:03:01 +0700

    sửa lỗi bị xoá dữ liệu khi đăng xuất

--- frontend/src/actions/logout.js
+++ frontend/src/actions/logout.js
@@ -1,18 +1,10 @@
 import { redirect } from "react-router-dom";
-import { deleteItem } from "../helpers";
 import { toast } from "react-toastify";
-export async function logoutAction() {
-  //delete user name from local storage
-  deleteItem({ 
-        key: "userName" 
-    });
-    deleteItem({ 
-      key: "budgets" 
-  });
-  deleteItem({ 
-    key: "expenses" 
-});
-  toast.success("User deleted");
 
+export async function logoutAction() {
+  // Display a success message
+  toast.success("User logged out");
+
+  // Redirect to the homepage without deleting local storage data
   return redirect("/");
 }
